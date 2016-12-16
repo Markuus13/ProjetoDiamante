66da075ad663806ec6936eda03dce298257d6bbf
Marcus Vinícius <marcus.f22@gmail.com>
2016-12-16 19:00:55 -0200

    Deixando os repositórios menos específicos

--- src/main/java/br/com/projetodiamante/repository/ClienteRepository.java
+++ src/main/java/br/com/projetodiamante/repository/ClienteRepository.java
@@ -1,11 +1,11 @@
 package br.com.projetodiamante.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.projetodiamante.model.Cliente;
 
 @Repository
-public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+public interface ClienteRepository extends CrudRepository<Cliente, Long> {
 
 }
--- src/main/java/br/com/projetodiamante/repository/VendaRepository.java
+++ src/main/java/br/com/projetodiamante/repository/VendaRepository.java
@@ -1,9 +1,11 @@
 package br.com.projetodiamante.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.projetodiamante.model.Venda;
 
-public interface VendaRepository extends JpaRepository<Venda, Long> {
+@Repository
+public interface VendaRepository extends CrudRepository<Venda, Long> {
 
 }
